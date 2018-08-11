d4eb2ec1552e56227e3b7cd1a02422677b3fd03d
Voon Shun Zhi <voonshunzhi@gmail.com>
2018-08-11 17:20:36 +0000

    Dop

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   process :resize_to_fill => [665, 375] # the defualt image size
   
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -4,8 +4,6 @@ class VideoUploader < CarrierWave::Uploader::Base
    include CarrierWave::MiniMagick
    process :resize_to_fill => [665, 375] # the defualt image size
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
