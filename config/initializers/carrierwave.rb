dbea9cbe9af9ecf9e4872b7b12f415e476b73d9f
Voon Shun Zhi <voonshunzhi@gmail.com>
2018-08-12 04:34:58 +0000

    Vhange s3 to aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,8 +16,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY_ID'],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY_ID'],
       region: 'eu-west-1',
       endpoint: 'https://s3.amazonaws.com'
     }
