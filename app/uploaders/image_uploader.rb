ac5c960386a1bf217c073bd8785be06e67638436
Voon Shun Zhi <voonshunzhi@gmail.com>
2018-08-12 11:03:22 +0000

    Done

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,11 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
    # the defualt image size
   
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.development? || Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+  storage :fog
+  storage :file
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -4,11 +4,8 @@ class VideoUploader < CarrierWave::Uploader::Base
   #include CarrierWave::MiniMagick
    # the defualt image size
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.development? || Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+  storage :fog
+  storage :file
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
