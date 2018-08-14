ce96aecf2dca72b7f9a326f55eeb74348b506675
Voon Shun Zhi <voonshunzhi@gmail.com>
2018-08-14 03:15:50 +0000

    Done

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -36,6 +36,7 @@ CarrierWave.configure do |config|
     provider:           'AWS',
     aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY_ID"],
+    region:'ap-southeast-1'
   }
   config.fog_directory  = 'vonflix'
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
